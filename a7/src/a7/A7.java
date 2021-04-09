e91bf6fcaae5557730bc7b01464d38dbfcea0b45
Andres <80366857+dandrespenagos@users.noreply.github.com>
2021-04-09 13:39:59 -0500

    ...

--- a7/src/a7/A7.java
+++ a7/src/a7/A7.java
@@ -3,11 +3,19 @@ package a7;
 
 public class A7 {
 
- 
+    /*
+    Autores:
+    -Diego Penagos
+    -Jhoan Nieto
+    -Valentina Rojas
+    -Zooyin Perez
+    
+    */
     
     public static void main(String[] args) {
         
-        System.out.println("Semana 7");
+        System.out.println("Semana");
+        
         
     }
     
